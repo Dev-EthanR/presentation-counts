5153664f132a0b12a1bf098c299bc23e6030ba7d
Ethan <ethanlegend7@gmail.com>
2026-01-12 17:13:53 +1100

    build comment section on home page

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -13,10 +13,22 @@ const Home = () => {
         <Services />
       </div>
       <Process />
-      <div className="my-6">
-        <GalleryBrief />
-      </div>
-      <h1 className="my-90 text-center text-5xl font-bold">Home goes here</h1>
+      <GalleryBrief />
+      <section className="text-center py-6">
+        <h2 className="text-xl lg:text-3xl font-bold mb-3">
+          No Upfront Fee Option Available
+        </h2>
+        <p className="text-sm max-w-80 lg:max-w-full lg:text-xl text-secondary font-semibold mb-4">
+          small initial deposit on quote acceptance, balance payable on
+          furniture removal (conditions apply).
+        </p>
+        <a
+          href="tel+61-394-822-722"
+          className="text-gray-900 text-base lg:text-2xl font-bold"
+        >
+          03 9482 2722
+        </a>
+      </section>
     </main>
   );
 };
